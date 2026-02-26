7d0f9de4f44d4d94cbd6d140e19558e398b2d631
aishwarya sriharsha <aishwaryamulkundkar98@gmail.com>
2026-02-26 17:50:31 +0530

    Some message

--- Conversation.sh
+++ Conversation.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-PERSON1=Simmy
-PERSON2=Meenu
+PERSON1=Jaya
+PERSON2=Vijaya
 
-PERSON2=Meenu
 echo "$PERSON1:: Hello $PERSON2"
 echo "$PERSON2:: Hello $PERSON1, How are you ?"
 echo "$PERSON1:: Iam good , How about you?"
